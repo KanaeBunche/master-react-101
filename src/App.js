ae518c8c367c3ea83cb71c9ddb5c4f279d05d236
Kanae <kanae.bunche@gmail.com>
2024-10-16 15:04:14 -0400

    skeloton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+
+
+export default function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+      <h1> </h1>
+      <img src="" alt=""/>
+      <h3> Followers: </h3>
+      <button> Unfollow </button>
+      <h3> Likes</h3>
     </div>
   );
 }
 
-export default App;
+
